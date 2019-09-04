c7b5d53e45ab9f3da6b6ff96c13c6d99a18dc755
jain <rohanpjain879@gmail.com>
2019-09-04 11:53:16 +0530

    FaceBookLoginIn callback error

---
+++ app/src/main/java/com/example/securityapplication/FaceBookLoginIn.java
@@ -0,0 +1,111 @@
+package com.example.securityapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.io.Serializable;
+
+import static com.facebook.AccessTokenManager.TAG;
+
+public class FaceBookLoginIn implements Serializable{
+
+    //private LoginResult loginResult;
+    private FirebaseAuth mAuth;
+    private Activity activity;
+    private FirebaseUser user;
+
+    private OnFaceBookLoginListener callback;
+    private static volatile FaceBookLoginIn faceBookInstance;
+
+    //private constructor
+    private FaceBookLoginIn(){
+        //Prevent form the reflection api.
+        if (faceBookInstance != null){
+            throw new RuntimeException("Use getInstance() method to get the single instance of this class.");
+        }
+    }
+
+    public void init(Activity activity, FirebaseAuth mAuth){
+
+                    this.activity = activity;
+                    this.mAuth = mAuth;
+                    //this.loginResult = loginResult;
+    }
+
+    public static FaceBookLoginIn getInstance() {
+        //Double check locking pattern
+        if (faceBookInstance == null) { //Check for the first time..if there is no instance available... create new one
+            synchronized (FaceBookLoginIn.class) { //Check for the second time to make ThreadSafe
+                //if there is no instance available... create new one
+                if (faceBookInstance == null)
+                    faceBookInstance = new FaceBookLoginIn();
+            }
+        }
+        return faceBookInstance;
+    }
+
+    //Make singleton from serialize and deserialize operation.
+    protected FaceBookLoginIn readResolve() {
+        return getInstance();
+    }
+
+
+    public void handleFacebookAccessToken(AccessToken token) {
+        Log.d(TAG, "handleFacebookAccessToken:" + token);
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(activity, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            setUser(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            setUser(null);
+                        }
+                    }
+                });
+    }
+
+    private void setUser(FirebaseUser user){
+        this.user = user;
+        callback.updateUI(user);
+    }
+
+    public boolean isLoggedIn(){
+        //hideProgressDialog();
+        if(this.user != null)
+            return true;
+        else
+            return false;
+    }
+
+
+    public void setOnFaceBookLoginListener(OnFaceBookLoginListener callback){
+        this.callback = callback;
+    }
+    public interface OnFaceBookLoginListener{
+        void updateUI(FirebaseUser user);
+    }
+}
--- app/src/main/java/com/example/securityapplication/MainActivity.java
+++ app/src/main/java/com/example/securityapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.securityapplication;
 
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -12,6 +13,13 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.support.annotation.NonNull;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -20,18 +28,25 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.Arrays;
 import java.util.concurrent.Executor;
 
 //import androidx.annotation.NonNull;
 
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, FaceBookLoginIn.OnFaceBookLoginListener{
 
     private FirebaseAuth mAuth;
     private TextView mStatus;
     private EditText mEmail;
     private EditText mPassword;
     private Button mSignInButton;
+
+    private LoginButton faceBookLoginButton;
+    private CallbackManager callbackManager;
+    private static final String EMAIL = "email";
+    //
+    private FaceBookLoginIn faceBookLoginIn;
 
     @Override
     public void onClick(View v) {
@@ -45,6 +60,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     Intent intent = new Intent(this, GoogleSignInActivity.class);
                     startActivity(intent);
                 }
+                AccessToken accessToken = AccessToken.getCurrentAccessToken();
+                boolean isLoggedInFaceBook = accessToken != null && !accessToken.isExpired();
+                if(isLoggedInFaceBook){
+                    LoginManager.getInstance().logOut();
+                }
+                updateUI(null);
             }
         }
         else if (i==R.id.googleSignInButton){
@@ -64,9 +85,42 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         FirebaseApp.initializeApp(this);
         mAuth=FirebaseAuth.getInstance();
 
+        faceBookLoginIn = FaceBookLoginIn.getInstance();
+        faceBookLoginIn.init(MainActivity.this, mAuth);
+
         findViewById(R.id.signInButton).setOnClickListener(this);
         findViewById(R.id.googleSignInButton).setOnClickListener(this);
 
+        callbackManager = CallbackManager.Factory.create();
+        faceBookLoginButton = findViewById(R.id.facebook_login_button);
+        faceBookLoginButton.setPermissions(Arrays.asList(EMAIL));
+
+        // Callback registration
+        faceBookLoginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d("FB", "facebook:onSuccess:" + loginResult);
+                faceBookLoginIn.setOnFaceBookLoginListener((FaceBookLoginIn.OnFaceBookLoginListener)faceBookLoginIn);
+                faceBookLoginIn.handleFacebookAccessToken(loginResult.getAccessToken());
+                FirebaseUser currentUser = mAuth.getCurrentUser();
+                if(faceBookLoginIn.isLoggedIn())
+                    updateUI(currentUser);
+                else
+                    updateUI(null);
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d("FB", "facebook:onCancel");
+                updateUI(null);
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                Log.d("FB", "facebook:onError", exception);
+                updateUI(null);
+            }
+        });
     }
 
     public void onStart(){
@@ -97,6 +151,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         return valid;
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Pass the activity result back to the Facebook SDK
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
     public void signIn(String email, String password){
 
         if(!validateForm()){
@@ -122,10 +184,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void signOut(){
         mAuth.signOut();
-        updateUI(null);
     }
 
-    private void updateUI(FirebaseUser user){
+    public void updateUI(FirebaseUser user){
         if(user==null){
             mStatus.setText(R.string.not_logged);
             mSignInButton.setText(R.string.sign_in_text);

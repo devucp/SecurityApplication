d155b2e17525e189a3108ebc3e36ccd6c07e4ac1
sachin <sachin.saw.13@gmail.com>
2019-09-10 20:12:49 +0530

    signup page 2 gui add

---
+++ app/src/main/java/com/example/securityapplication/activity_signup2.java
@@ -0,0 +1,13 @@
+package com.example.securityapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_signup2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup2);
+    }
+}

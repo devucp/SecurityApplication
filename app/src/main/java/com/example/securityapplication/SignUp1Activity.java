31b67ffe9daf32c76eb2e627bf6b0cae734cebc4
sachin <sachin.saw.13@gmail.com>
2019-09-10 14:48:19 +0530

    first security app

--- app/src/main/java/com/example/securityapplication/SignUp1Activity.java
+++ app/src/main/java/com/example/securityapplication/SignUp1Activity.java
@@ -1,6 +1,8 @@
 package com.example.securityapplication;
 
+import android.app.DatePickerDialog;
 import android.database.Cursor;
+import android.icu.util.Calendar;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -14,19 +16,22 @@ import android.widget.Toast;
 
 public class SignUp1Activity extends AppCompatActivity {
 
-    Database_Helper myDb;
-    Validation val = new Validation();
+   // Database_Helper myDb;
+    //Validation val = new Validation();
     private TextView text_view;
     private TextInputLayout textinputName,textinputDOB,textinputEmail,textinputPass,textinputCnfPass;
     private RadioGroup gender_grp;
     private RadioButton Radio_Gender;
+   // private DatePickerDialog.OnDateSetListener setListener;
     private Button Btn_Submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup1);
-        myDb = new Database_Helper(this);
+       // myDb = new Database_Helper(this);
+       // java.util.Calendar calendar=Calendar.getInstance();
+      //final int year=calendar.get(Calendar.YEAR);
 
         text_view = (TextView)findViewById(R.id.text_gender);
         textinputName = findViewById(R.id.textlayout_Name);
@@ -39,50 +44,50 @@ public class SignUp1Activity extends AppCompatActivity {
 
     }
 
-
-    private void ShowMessage(String title,String Message){
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setCancelable(true);
-        builder.setTitle(title);
-        builder.setMessage(Message);
-    }
-
-    public void Validater(View view) {
-        if (val.validateName(textinputName) & val.validateGender(gender_grp,text_view) & val.validateDob(textinputDOB) & val.validateEmail(textinputEmail) &
-                val.validatePassword(textinputPass) & val.validateCnfPassword(textinputPass,textinputCnfPass)){
-            AddData();
-        }
-        else {
-            Toast.makeText(this,"Enter Valid Credentials",Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    private void AddData() {
-        int selected_id = gender_grp.getCheckedRadioButtonId();
-        Radio_Gender = (RadioButton) findViewById(selected_id);
-        String gender = Radio_Gender.getText().toString().trim();
-        Boolean isInserted = myDb.insert_data(textinputName.getEditText().getText().toString().trim(),
-                gender,
-                textinputDOB.getEditText().getText().toString().trim(),
-                textinputEmail.getEditText().getText().toString().trim(),
-                textinputPass.getEditText().getText().toString().trim());
-        if (isInserted) {
-            textinputName.getEditText().setText(null);
-            gender_grp.clearCheck();
-            textinputDOB.getEditText().setText(null);
-            textinputEmail.getEditText().setText(null);
-            textinputPass.getEditText().setText(null);
-            textinputCnfPass.getEditText().setText(null);
-        }
-        else {
-            String UserEmail = textinputEmail.getEditText().getText().toString().trim();
-            boolean res = myDb.CheckUserEmail(UserEmail);
-            if (res){
-                Toast.makeText(this,"Email already taken",Toast.LENGTH_SHORT).show();
-            }
-            else{
-                Toast.makeText(this,"User Entry Unsuccessful",Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
+//
+//    private void ShowMessage(String title,String Message){
+//        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+//        builder.setCancelable(true);
+//        builder.setTitle(title);
+//        builder.setMessage(Message);
+//    }
+//
+//    public void Validater(View view) {
+//        if (val.validateName(textinputName) & val.validateGender(gender_grp,text_view) & val.validateDob(textinputDOB) & val.validateEmail(textinputEmail) &
+//                val.validatePassword(textinputPass) & val.validateCnfPassword(textinputPass,textinputCnfPass)){
+//            AddData();
+//        }
+//        else {
+//            Toast.makeText(this,"Enter Valid Credentials",Toast.LENGTH_SHORT).show();
+//        }
+//    }
+//
+//    private void AddData() {
+//        int selected_id = gender_grp.getCheckedRadioButtonId();
+//        Radio_Gender = (RadioButton) findViewById(selected_id);
+//        String gender = Radio_Gender.getText().toString().trim();
+//        Boolean isInserted = myDb.insert_data(textinputName.getEditText().getText().toString().trim(),
+//                gender,
+//                textinputDOB.getEditText().getText().toString().trim(),
+//                textinputEmail.getEditText().getText().toString().trim(),
+//                textinputPass.getEditText().getText().toString().trim());
+//        if (isInserted) {
+//            textinputName.getEditText().setText(null);
+//            gender_grp.clearCheck();
+//            textinputDOB.getEditText().setText(null);
+//            textinputEmail.getEditText().setText(null);
+//            textinputPass.getEditText().setText(null);
+//            textinputCnfPass.getEditText().setText(null);
+//        }
+//        else {
+//            String UserEmail = textinputEmail.getEditText().getText().toString().trim();
+//            boolean res = myDb.CheckUserEmail(UserEmail);
+//            if (res){
+//                Toast.makeText(this,"Email already taken",Toast.LENGTH_SHORT).show();
+//            }
+//            else{
+//                Toast.makeText(this,"User Entry Unsuccessful",Toast.LENGTH_SHORT).show();
+//            }
+//        }
+ //   }
 }

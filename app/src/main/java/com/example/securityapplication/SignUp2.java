caf66bffa82f9dfd46a6316c3160419be93700a4
divyanshu <dgdivyanshu@gmail.com>
2019-09-14 00:21:15 +0530

    COnverted nested if-else to single if else and corrected checking logic

--- app/src/main/java/com/example/securityapplication/SignUp2.java
+++ app/src/main/java/com/example/securityapplication/SignUp2.java
@@ -88,26 +88,29 @@ public class SignUp2 extends AppCompatActivity {
                 boolean valid_location = false;
                 boolean empty_mobile = inputValidation.is_Empty(input_mobile, "PLEASE ENTER MOBILE NO. ");
                 if (!empty_mobile) {
-                    if (!inputValidation.valid_input(input_mobile, 10, getString(R.string.no_phone))) {
-                        if (inputValidation.is_Valid(input_mobile)) {
-                            input_mobile.setError(getString(R.string.no_phone));
-                        }
+                    if (!inputValidation.isMinLength(input_mobile, 10, getString(R.string.no_phone)) || !inputValidation.is_numeric(input_mobile)) {
+
+                        input_mobile.setError(getString(R.string.no_phone));
+
                     }
-                } else{
-                    valid_mobile = true;
-                    input_mobile.setError(null);
+                     else {
+                        valid_mobile = true;
+                        input_mobile.setError(null);
+                    }
                 }
 
                 boolean empty_aadhar = inputValidation.is_Empty(input_aadhar, getString(R.string.no_aadhar));
                 if (!empty_aadhar) {
-                    if (!inputValidation.valid_input(input_aadhar, 12, getString(R.string.no_aadhar))) {
-                        if (inputValidation.is_Valid(input_aadhar)) {
-                            input_aadhar.setError(getString(R.string.no_aadhar));
+                    if (!inputValidation.isMinLength(input_aadhar, 12, getString(R.string.no_aadhar)) ||
+                            !inputValidation.is_numeric(input_aadhar)) {
+
+                                input_aadhar.setError(getString(R.string.no_aadhar));
                         }
+
+                    else {
+                        valid_aadhar = true;
+                        input_aadhar.setError(null);
                     }
-                } else {
-                    valid_aadhar = true;
-                    input_aadhar.setError(null);
                 }
 
                 if (input_location.getText().toString().isEmpty()) {
@@ -125,8 +128,14 @@ public class SignUp2 extends AppCompatActivity {
 
                 //Moved IMEI reading code to this place so IMEI can be stored for user object
                 boolean empty = inputValidation.all_Empty(input_mobile,input_aadhar,input_location,getString(R.string.message));
+
+                Log.d("SIgnUP2"," Valid Aadhar:"+valid_aadhar+" Valid Mobile:"+valid_mobile+" Valid Location:"+valid_location);
                 boolean valid = valid_mobile && valid_aadhar && valid_location;
-                if(!empty){
+
+                Log.d("SIgnUP2","Empty:"+empty+" Valid"+valid);
+
+                //NOTE: Allow database entry only if not empty AND valid
+                if(!empty && valid){
                     String imei = null;
                     TelephonyManager tm = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
                     String permission = Manifest.permission.READ_PHONE_STATE;
@@ -137,7 +146,10 @@ public class SignUp2 extends AppCompatActivity {
                             Log.d("IMEI", "IMEI Number of slot 1 is:" + imei);
 
                         } else {
+                            imei=tm.getDeviceId();
                             Log.d("SignUp2", "SDK Version not of required level");
+                            Log.d("SignUp2", "Using getDeviceId()"+imei);
+
                         }
                     } else {
                         Log.d("SIgnUP2", "PERMISSION FOR READ STATE NOT GRANTED, REQUESTING PERMSISSION...");
@@ -146,7 +158,8 @@ public class SignUp2 extends AppCompatActivity {
                     }
 
 
-                    if (DBHelper.checkUser(input_aadhar.getText().toString().trim()) && !empty){
+                    if (DBHelper.checkUser(input_aadhar.getText().toString().trim()) ){
+
                         user.setMobile(input_mobile.getText().toString().trim());
                         user.setAadhar(input_aadhar.getText().toString().trim());
                         user.setLocation(input_location.getText().toString().trim());

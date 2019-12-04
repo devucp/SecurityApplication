d957128da9869fb3be4af2648cd8a39975836eae
divyanshu <dgdivyanshu@gmail.com>
2019-12-04 15:02:59 +0530

    Added send sms feature with dummy contact

---
+++ app/src/main/java/com/example/securityapplication/RestartJobService.java
@@ -0,0 +1,35 @@
+package com.example.securityapplication;
+
+import android.app.job.JobParameters;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Build;
+import android.os.Handler;
+import android.util.Log;
+import android.app.job.JobService;
+
+public class RestartJobService extends JobService{
+        private static String TAG= JobService.class.getSimpleName();
+        private static RestartServiceBroadcastReceiver restartSensorServiceReceiver;
+        private static JobService instance;
+        private static JobParameters jobParameters;
+
+        @Override
+        public boolean onStartJob(JobParameters jobParameters) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                startForegroundService(new Intent(this,SosPlayer.class));
+            } else {
+                startService(new Intent(this,SosPlayer.class));
+            }
+            instance= this;
+            RestartJobService.jobParameters= jobParameters;
+
+            return false;
+        }
+
+    @Override
+    public boolean onStopJob(JobParameters params) {
+        return false;
+    }
+}

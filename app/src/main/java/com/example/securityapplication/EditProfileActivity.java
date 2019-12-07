f5c1c71a9b02f9b5a09e4e99f3f0117d35db31e3
kaushty <kaushikshetty4898@gmail.com>
2019-12-07 11:45:29 +0530

    Made Changes to Profile Activity

--- app/src/main/java/com/example/securityapplication/EditProfileActivity.java
+++ app/src/main/java/com/example/securityapplication/EditProfileActivity.java
@@ -30,7 +30,7 @@ public class EditProfileActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_profile);
+        setContentView(R.layout.profile_activity_edit);
 
         mydb = new SQLiteDBHelper(this);
         val = new Validation();
--- app/src/main/java/com/example/securityapplication/ProfileActivity.java
+++ app/src/main/java/com/example/securityapplication/ProfileActivity.java
@@ -22,7 +22,7 @@ public class ProfileActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        setContentView(R.layout.fragment_profile);
 
 
         initObjects();

a53f3dd3a4484ea61aaba9cfac81b66da09df047
sachinsav <sachin.sav.13@gmail.com>
2019-12-09 13:33:29 +0530

    gui some update like contact picker and many more

--- app/src/main/java/com/example/securityapplication/home_fragment.java
+++ app/src/main/java/com/example/securityapplication/home_fragment.java
@@ -1,5 +1,9 @@
 package com.example.securityapplication;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,11 +11,55 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class home_fragment extends Fragment {
+    Button bt;
+    Boolean is_paid=false;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home,container,false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        bt=getActivity().findViewById(R.id.emergency);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(is_paid)
+                {
+                    Toast.makeText(getContext(), "You are premier member", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    //if user using free services only
+                    new AlertDialog.Builder(getContext()).setMessage("Upgrade to Premier to Use this function")
+                            .setPositiveButton("Purchased", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    try{
+                                        Intent intent = new Intent();
+                                        intent.setAction(Intent.ACTION_VIEW);
+                                        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                                        intent.setData(Uri.parse("http://www.w3schools.com"));
+                                        startActivity(intent);
+                                    }
+                                    catch (Exception e)
+                                    {
+                                        Toast.makeText(getContext(), "Something went wrong", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            }).setNegativeButton("Cancel", null).setCancelable(false).create().show();
+
+
+                }
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/securityapplication/sos_page.java
+++ app/src/main/java/com/example/securityapplication/sos_page.java
@@ -1,16 +1,31 @@
 package com.example.securityapplication;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.provider.ContactsContract.Contacts;
+import android.provider.ContactsContract.CommonDataKinds.Email;
 import android.content.Intent;
+import android.provider.ContactsContract;
+import android.support.annotation.Nullable;
 import android.support.design.widget.TextInputEditText;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class sos_page extends AppCompatActivity {
+    Intent intent;
+    public  static final int RequestPermissionCode  = 1 ;
     Button save, edit;
     String n1 = "1123456789";
     String n2 = "";
@@ -18,7 +33,8 @@ public class sos_page extends AppCompatActivity {
     String n4 = "";
     String n5 = "";
 
-    TextInputEditText c1, c2, c3, c4, c5;
+    TextInputEditText c1, c2, c3, c4, c5,current;
+    private final static int CONTACT_PICKER_RESULT = 1001;
 
 
     @Override
@@ -48,6 +64,98 @@ public class sos_page extends AppCompatActivity {
         c3.setEnabled(false);
         c4.setEnabled(false);
         c5.setEnabled(false);
+        c1.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                final int DRAWABLE_LEFT = 0;
+                final int DRAWABLE_TOP = 1;
+                final int DRAWABLE_RIGHT = 2;
+                final int DRAWABLE_BOTTOM = 3;
+
+                if(event.getAction() == MotionEvent.ACTION_UP) {
+                    if(event.getRawX() >= (c1.getRight() - c1.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
+                        current=c1;
+                        intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+                        startActivityForResult(intent, 7);
+                    }
+                }
+                return false;
+            }
+        });
+        c2.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                final int DRAWABLE_LEFT = 0;
+                final int DRAWABLE_TOP = 1;
+                final int DRAWABLE_RIGHT = 2;
+                final int DRAWABLE_BOTTOM = 3;
+
+                if(event.getAction() == MotionEvent.ACTION_UP) {
+                    if(event.getRawX() >= (c2.getRight() - c2.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
+                        current=c2;
+                        intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+                        startActivityForResult(intent, 7);
+                    }
+                }
+                return false;
+            }
+        });
+        c3.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                final int DRAWABLE_LEFT = 0;
+                final int DRAWABLE_TOP = 1;
+                final int DRAWABLE_RIGHT = 2;
+                final int DRAWABLE_BOTTOM = 3;
+
+                if(event.getAction() == MotionEvent.ACTION_UP) {
+                    if(event.getRawX() >= (c3.getRight() - c3.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
+                        current=c3;
+                        intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+                        startActivityForResult(intent, 7);
+                    }
+                }
+                return false;
+            }
+        });
+        c4.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                final int DRAWABLE_LEFT = 0;
+                final int DRAWABLE_TOP = 1;
+                final int DRAWABLE_RIGHT = 2;
+                final int DRAWABLE_BOTTOM = 3;
+
+                if(event.getAction() == MotionEvent.ACTION_UP) {
+                    if(event.getRawX() >= (c4.getRight() - c4.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
+                        current=c4;
+                        intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+                        startActivityForResult(intent, 7);
+                    }
+                }
+                return false;
+            }
+        });
+        c5.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                final int DRAWABLE_LEFT = 0;
+                final int DRAWABLE_TOP = 1;
+                final int DRAWABLE_RIGHT = 2;
+                final int DRAWABLE_BOTTOM = 3;
+
+                if(event.getAction() == MotionEvent.ACTION_UP) {
+                    if(event.getRawX() >= (c5.getRight() - c5.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
+                        current=c5;
+                        intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+                        startActivityForResult(intent, 7);
+                    }
+                }
+                return false;
+            }
+        });
+
+
 
         save.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -63,6 +171,7 @@ public class sos_page extends AppCompatActivity {
                         c4.setEnabled(false);
                         c5.setEnabled(false);
                         Toast.makeText(sos_page.this, "Contact saved successfully", Toast.LENGTH_SHORT).show();
+                        edit.setBackground(getResources().getDrawable(R.drawable.btn_cus));
                         Intent intent;
                         intent = new Intent(sos_page.this,navigation.class);
                         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK |Intent.FLAG_ACTIVITY_CLEAR_TOP);
@@ -81,6 +190,8 @@ public class sos_page extends AppCompatActivity {
         edit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                edit.setBackground(getResources().getDrawable(R.drawable.btn_cus_edit));
             c1.setEnabled(true);
             c2.setEnabled(true);
             c3.setEnabled(true);
@@ -90,6 +201,92 @@ public class sos_page extends AppCompatActivity {
             }
         });
 
+    }
+    public void EnableRuntimePermission(){
+
+        if (ActivityCompat.shouldShowRequestPermissionRationale(sos_page.this,
+                Manifest.permission.READ_CONTACTS))
+        {
+
+            Toast.makeText(sos_page.this,"CONTACTS permission allows us to Access CONTACTS app", Toast.LENGTH_LONG).show();
+
+        } else {
+
+            ActivityCompat.requestPermissions(sos_page.this,new String[]{
+                    Manifest.permission.READ_CONTACTS}, RequestPermissionCode);
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int RC, String per[], int[] PResult) {
+
+        switch (RC) {
+
+            case RequestPermissionCode:
+
+                if (PResult.length > 0 && PResult[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    Toast.makeText(sos_page.this,"Permission Granted, Now your application can access CONTACTS.", Toast.LENGTH_LONG).show();
+
+                } else {
+
+                    Toast.makeText(sos_page.this,"Permission Canceled, Now your application cannot access CONTACTS.", Toast.LENGTH_LONG).show();
+
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void onActivityResult(int RequestCode, int ResultCode, Intent ResultIntent) {
+
+        super.onActivityResult(RequestCode, ResultCode, ResultIntent);
+
+        switch (RequestCode) {
+
+            case (7):
+                if (ResultCode == Activity.RESULT_OK) {
+
+                    Uri uri;
+                    Cursor cursor1, cursor2;
+                    String TempNameHolder, TempNumberHolder, TempContactID, IDresult = "" ;
+                    int IDresultHolder ;
+
+                    uri = ResultIntent.getData();
+
+                    cursor1 = getContentResolver().query(uri, null, null, null, null);
+
+                    if (cursor1.moveToFirst()) {
+
+                        TempNameHolder = cursor1.getString(cursor1.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+
+                        TempContactID = cursor1.getString(cursor1.getColumnIndex(ContactsContract.Contacts._ID));
+
+                        IDresult = cursor1.getString(cursor1.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER));
+
+                        IDresultHolder = Integer.valueOf(IDresult) ;
+
+                        if (IDresultHolder == 1) {
+
+                            cursor2 = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = " + TempContactID, null, null);
+
+                            while (cursor2.moveToNext()) {
+
+                                TempNumberHolder = cursor2.getString(cursor2.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                                Log.i("contact_name",TempNameHolder);
+                                int len=TempNumberHolder.length();
+                                String tmp;
+                                tmp=TempNumberHolder.substring(len-10,len);
+                                current.setText(tmp);
+
+                            }
+                        }
+
+                    }
+                }
+                break;
+        }
     }
 
 

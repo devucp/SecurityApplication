80013e0666addf1fe7343fa327db318db788613f
divyanshu <dgdivyanshu@gmail.com>
2019-12-24 15:46:58 +0530

    Added a SettingsContentObserver register and unregister

--- app/src/main/java/com/example/securityapplication/BackgroundSosPlayerService.java
+++ app/src/main/java/com/example/securityapplication/BackgroundSosPlayerService.java
@@ -2,13 +2,18 @@ package com.example.securityapplication;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.os.Handler;
 import android.os.IBinder;
 
 
 public class BackgroundSosPlayerService extends Service {
     private static final String TAG = null;
     MediaPlayer player;
+    AudioManager mAudioManager;
+    SettingsContentObserver mSettingsContentObserver;
     public IBinder onBind(Intent arg0) {
 
         return null;
@@ -16,13 +21,22 @@ public class BackgroundSosPlayerService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
+        mAudioManager= (AudioManager)getSystemService(AUDIO_SERVICE);
+        mAudioManager.setMode(AudioManager.STREAM_MUSIC);
+        mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC,mAudioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC),0);
+        mAudioManager.setSpeakerphoneOn(true);
+
         player = MediaPlayer.create(this, R.raw.idil);
         player.setLooping(true); // Set looping
-        player.setVolume(100,100);
+        //player.setVolume(100,100);
+
+
 
     }
     public int onStartCommand(Intent intent, int flags, int startId) {
-        player.start();
+       player.start();
+        mSettingsContentObserver = new SettingsContentObserver(this,new Handler());
+        getApplicationContext().getContentResolver().registerContentObserver(android.provider.Settings.System.CONTENT_URI, true, mSettingsContentObserver );
         return 1;
     }
 
@@ -42,6 +56,7 @@ public class BackgroundSosPlayerService extends Service {
     }
     @Override
     public void onDestroy() {
+        getApplicationContext().getContentResolver().unregisterContentObserver(mSettingsContentObserver);
         player.stop();
         player.release();
     }

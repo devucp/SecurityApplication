a95f9408db75704c796ce21c58b727ceca7d5b92
kaushty <kaushikshetty4898@gmail.com>
2019-12-30 11:41:12 +0530

    minor changes added a toasty and a log

--- app/src/main/java/com/example/securityapplication/profile_fragment.java
+++ app/src/main/java/com/example/securityapplication/profile_fragment.java
@@ -610,12 +610,13 @@ public class profile_fragment extends Fragment {
             Intent mStopSosPlayer=new Intent(getContext(),SosPlayer.class);
             mStopSosPlayer.putExtra("stop",1);
             getActivity().startService(mStopSosPlayer); //previously was stopService(). Now using startService() to use the stop extra in onStartCommand()
-            Log.d("Profile Fr","Service sosplayer new startIntent...");
+            Log.d("Profile Fragment","Service sosplayer new startIntent...");
 
             //to stop GetGPS Service upon logout
             Intent MStopGPSService = new Intent(getContext(),GetGPSCoordinates.class);
             getActivity().stopService(MStopGPSService);
-            Toasty.warning(getContext(), "Service sosplayer stopping...", Toast.LENGTH_SHORT, true).show();
+            Log.d("Profile Fragment","GPS Service Stopped");
+            Toasty.warning(getContext(), "Services Stopped", Toast.LENGTH_SHORT, true).show();
 
            // Toast.makeText(getContext(),"Service sosplayer stopping...",Toast.LENGTH_SHORT).show();
         }
